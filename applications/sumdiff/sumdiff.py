86417455cce10e22f2f4bb367d48ded22a18a7fa
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-05 12:53:32 -0700

    solved summdiff, but inefficient

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -13,3 +13,41 @@ def f(x):
 
 # Your code here
 
+"""
+for n in q:
+    sum each pair, store in dict (ie 1+3 == d[(0,1)] = 4)
+
+for n in q:
+    diff each pair, return if match, or return None
+"""
+def find_matches(l):
+    result = []
+    d = {}
+    e = {}
+    # TODO: Efficiency
+    # get the index and value of each node in q and add/subtract, storing in separate dictionaries with keys as tuples of keys and value as value
+    for i, n in enumerate(l):
+        for j, o in enumerate(l):        
+            key = i
+            next_key = j
+            
+            next_key = j
+            next_value = o
+            # subtract no matter what
+            e[(key, next_key)] = n - next_value
+            # addition will have the same result with inverse keys, so avoid extra work
+            if next_key != None:
+                if (next_key, key) not in d.keys():
+                    d[(key, next_key)] = n + next_value               
+
+            #print(d)
+            #what I find really interesting here is what I expect to be the value at each position is actually the key. If I do k, v in d, I get the index and key where I would expect the key and value
+    for v in d:
+        for v2 in e:
+            if d[v] == e[v2]:
+                #print(f"match\nkey1: {v}\nkey2: {v2}")
+                #print(f"value1: {d[v]}\nvalue2: {e[v2]}\n")
+                result.append((v, v2))
+    return result
+
+print(find_matches(q))

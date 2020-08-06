d4a5d70099199695ef4363d2f0eb9b8cdfcf25fa
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-05 19:59:00 -0700

    test failing with whitespace

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,23 @@
 def word_count(s):
-    # Your code here
+    d = {}
+    phrase = ""
+
+    chars_to_ignore = '":;,.-+=/\\|[]{}()*^&'
+    
+    for char in s:
+        if char not in chars_to_ignore:
+            phrase += char
+
+    words = phrase.lower().split()
+
+    # count each word
+    for i in range(len(words)):        
+        if words[i] not in d:            
+            d[words[i]] = 0
+        else:
+            d[words[i]] += 1
+
+    return d
 
 
 
